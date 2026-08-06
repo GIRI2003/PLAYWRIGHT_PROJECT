ec8273383dc3d9e004a2634ede30ca2937f0c51a
GIRI2003 <girirohi2@gmail.com>
2026-08-06 17:44:48 +0530

    first commit

---
+++ src/JavaScriptLearning/Identifiers.js
@@ -0,0 +1,103 @@
+/* 
+
+Var, Let, and Const in JavaScript. They are used to declare variables in JavaScript. 
+The main differences between them are their scope, hoisting behavior, 
+and whether they can be updated or redeclared.
+
+
+We can use camelCase or PascalCase or snake_case or SREAMING_SNAKE_CASE for constant variable names.
+
+*/
+
+//1. var
+
+var x = 1; 
+/* 
+var --> Keyword or Identifier
+x --> variable name 
+= --> Assignment Operator
+1 --> Value
+*/
+
+console.log('Value of x: ', x); //1
+console.log('Data Type of x: ', typeof (x)); //number
+
+var x = 5;
+var x = 10; //Redeclaration is allowed
+console.log('Value of x: ', x); //10
+
+x = 15; //Updating is allowed
+console.log('Value of x: ', x); //15
+
+//2. let
+// Variables declared with let are block-scoped.
+// They can be updated but not redeclared in the same scope.
+let y = 20;
+console.log('Value of y: ', y); //20
+console.log('Data Type of y: ', typeof (y)); //number
+
+//let y = 25; //Redeclaration is not allowed
+y = 30; //Updating is allowed
+console.log('Value of y: ', y); //30
+
+//3. const
+// Variables declared with const are block-scoped.
+// They must be initialized at the time of declaration and cannot be updated or redeclared.
+
+const z = 40;
+console.log('Value of z: ', z); //40
+console.log('Data Type of z: ', typeof (z)); //number
+
+//const z = 45; //Redeclaration is not allowed
+//z = 50; //Updating is not allowed
+
+/* ------------------------------------------ */
+
+const PI = 3.14; //Constant variable
+//PI = 3.14159; ---> //Updating is not allowed
+console.log('Value of PI: ', PI); //3.14
+
+const my_name = "Girinath"; //Constant variable
+console.log('Value of my_name: ', my_name); //Girinath
+
+/* ------------------------------------------ */
+
+//Hoisting behavior of var, let, and const
+
+//1. var -> Hoisting is allowed. The variable is hoisted to the top of its scope and initialized with undefined.
+console.log('Value of a: ', a); //undefined
+var a = 100;
+
+//2. let -> Hoisting is not allowed. The variable is hoisted to the top of its scope but not initialized.
+//console.log('Value of b: ', b); //ReferenceError: Cannot access 'b' before initialization
+let b = 200;
+console.log('value of b: ', b); //200
+
+//3. const -> Hoisting is not allowed. The variable is hoisted to the top of its scope but not initialized.
+//console.log('Value of c: ', c); //ReferenceError: Cannot access 'c' before initialization
+const c = 300;
+console.log('Value of c: ', c); //300
+
+/* ------------------------------------------ */
+
+let g = 10;
+var g = 20; //Error: Redeclaration is allowed for var but not for let
+console.log(g); //20
+
+/* ------------------------------------------ */
+
+var h = 10; //SyntaxError: Identifier 'h' has already been declared
+let h = 20;
+console.log(h);
+
+/* ------------------------------------------ */
+
+i = 10; //ReferenceError: Cannot access 'i' before initialization
+let i = 20;
+console.log(i);
+
+/* ------------------------------------------ */
+
+j = 10;
+var j = 20;
+console.log(j); //20
---
+++ src/JavaScriptLearning/PrimitiveDataType.js
@@ -0,0 +1,119 @@
+//This File is all about the primitive data types in JavaScript. There are 7 primitive data types in JavaScript. They are:
+
+//1. Number
+
+/* Number is a primitive data type in JavaScript that represents numeric values. 
+It can be an integer or a floating-point number. JavaScript uses the IEEE 754 standard for representing 
+numbers, which means that all numbers are stored as 64-bit floating-point values. */
+
+let x = 10; //Postive Number
+console.log('Value of x: ',x); //10
+console.log('Data Type of x: ', typeof x); //Number
+console.log('Data Type of x: ', typeof (x)); //Number
+
+let y = 12.99; //Decimal Number
+console.log('Value of y: ',y); //12.99
+console.log('Data Type of y: ', typeof (y)); //Number
+
+let z = -1000; //Negative Number
+console.log('Value of z: ',z); //-1000
+console.log('Data Type of z: ', typeof (z)); //Number
+
+let xyz = 0xFF; //Hexadecimal
+console.log('Value of xyz: ',xyz); //255
+console.log('Data Type of xyz: ', typeof (xyz)); //Number
+
+let abc = 0b1010; //Binary
+console.log('Value of abc: ',abc); //10
+console.log('Data Type of abc: ', typeof (abc)); //Number
+
+//2. String
+
+/*String is a sequence of characters enclosed in single quotes, double quotes, or backticks. 
+It can contain letters, numbers, symbols, and spaces. Strings are used to represent text in JavaScript.
+Size for each character in a string is 2 bytes and takes extra 2 bytes for the length of the string. 
+So, the total size of a string is (2 * number of characters) + 2 bytes.
+*/
+
+let str1 = "Hello, World!"; //String with double quotes
+console.log('Value of str1: ',str1); //Hello, World!
+console.log('Data Type of str1: ', typeof (str1)); //String
+
+let str2 = 'Hello, JavaScript!'; //String with single quotes
+console.log('Value of str2: ',str2);  //Hello, JavaScript!
+console.log('Data Type of str2: ', typeof (str2)); //String
+
+let str3 = `${str1}`; //String with backticks (Template Literals)
+console.log('Value of str3: ',str3);  //Hello, World!
+console.log('Data Type of str3: ', typeof (str3)); //String
+
+let str4 = '1000'; //String with numbers
+console.log('Value of str4: ',str4);  //1000
+console.log('Data Type of str4: ', typeof (str4)); //String
+
+let str5 = 'true'; //String with boolean value
+console.log('Value of str5: ',str5);  //true
+console.log('Data Type of str5: ', typeof (str5)); //String
+
+let str6 = '$'; //String with special character
+console.log('Value of str6: ',str6);  //$
+console.log('Data Type of str6: ', typeof (str6)); //String
+
+//3. Boolean
+
+/* 
+Boolean is a primitive data type in JavaScript that represents one of two values: true or false.
+Each boolean value is stored in 4 bytes of memory.
+
+*/
+
+let bool1 = true; //Boolean value true
+console.log('Value of bool1: ',bool1); //true
+console.log('Data Type of bool1: ', typeof (bool1)); //Boolean
+
+let bool2 = false; //Boolean value false
+console.log('Value of bool2: ',bool2); //false
+console.log('Data Type of bool2: ', typeof (bool2)); //Boolean
+
+//4. Undefined
+
+/*
+Undefined is a primitive data type in JavaScript that represents the absence of a value or an uninitialized variable.
+When a variable is declared but not assigned a value, it is automatically assigned the value undefined.
+Each undefined value is stored in 4 bytes of memory.
+*/
+
+let undef; //Variable declared but not assigned a value
+console.log('Value of undef: ',undef); //undefined
+console.log('Data Type of undef: ', typeof (undef)); //Undefined
+
+undef = 10; //Modified the value of undef to 10
+console.log('Value of undef: ',undef); //10
+console.log('Data Type of undef: ', typeof (undef)); //Number
+
+let undef2 = undefined; //Variable declared and assigned a value of undefined
+console.log('Value of undef2: ',undef2); //undefined
+console.log('Data Type of undef2: ', typeof (undef2)); //Undefined
+
+undef2 = "Hello"; //Modified the value of undef2 to "Hello"
+console.log('Value of undef2: ',undef2); //Hello
+console.log('Data Type of undef2: ', typeof (undef2)); //String
+
+//5. Null
+
+/*
+Null is a primitive data type in JavaScript that represents the intentional absence of any object value.
+It is often used to indicate that a variable should have no value or that an object reference is empty.
+Each null value is stored in 4 bytes of memory.
+*/
+
+let nullVar = null; //Variable declared and assigned a value of null
+console.log('Value of nullVar: ',nullVar); //null
+console.log('Data Type of nullVar: ', typeof (nullVar)); //Object
+
+nullVar = 20; //Modified the value of nullVar to 20
+console.log('Value of nullVar: ',nullVar); //20
+console.log('Data Type of nullVar: ', typeof (nullVar)); //Number
+
+//6. BigInt
+//7. Symbol
